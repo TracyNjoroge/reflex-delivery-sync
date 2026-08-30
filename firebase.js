d6512b4f9fe8137c61efa604cb5dfc3423ff33c9
TheRealMilkah <michiramilkah@gmail.com>
2026-08-30 23:26:26 +0300

    Refactor Firebase initialization and add database methods
    Updated Firebase imports to use CDN links and added database functionalities.

--- firebase.js
+++ firebase.js
@@ -1,23 +1,37 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+    getDatabase,
+    ref,
+    push,
+    set,
+    onValue,
+    update
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAwtJj_eRuROtBoLdek-TLxLqRC4lOc3cM",
-  authDomain: "reflex-779d1.firebaseapp.com",
-  databaseURL: "https://reflex-779d1-default-rtdb.firebaseio.com",
-  projectId: "reflex-779d1",
-  storageBucket: "reflex-779d1.firebasestorage.app",
-  messagingSenderId: "863266679437",
-  appId: "1:863266679437:web:48ae4e8889632a7391d0c4",
-  measurementId: "G-QRW0KBDJR6"
-  export { app };
+    apiKey: "AIzaSyAwtJj_eRuROtBoLdek-TLxLqRC4lOc3cM",
+    authDomain: "reflex-779d1.firebaseapp.com",
+    databaseURL: "https://reflex-779d1-default-rtdb.firebaseio.com",
+    projectId: "reflex-779d1",
+    storageBucket: "reflex-779d1.firebasestorage.app",
+    messagingSenderId: "863266679437",
+    appId: "1:863266679437:web:48ae4e8889632a7391d0c4",
+    measurementId: "G-QRW0KBDJR6"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+
+
+export {
+    db,
+    ref,
+    push,
+    set,
+    onValue,
+    update
+};
